02a9e19ee2b1cefe905686dbb6f8e1c549f042a2
mphancock <matthewhancock97@gmail.com>
2025-01-16 21:30:42 -0600

    added functionality to test position opening

--- src/main.rs
+++ src/main.rs
@@ -19,6 +19,11 @@ use utils::{
     display_position_balances, display_wallet_balances, fetch_mint, fetch_position, fetch_whirlpool,
 };
 
+use orca_whirlpools::{
+    close_position_instructions, open_position_instructions, IncreaseLiquidityParam,
+};
+
+
 #[tokio::main]
 async fn main() {
     let args = Args::parse();
@@ -85,19 +90,97 @@ async fn main() {
     .await
     .expect("Failed to display position balances.");
 
-    loop {
-        if let Err(err) = run_position_manager(
-            &rpc,
-            &args,
-            &wallet,
-            &mut position,
-            &token_mint_a,
-            &token_mint_b,
-        )
-        .await
-        {
-            eprintln!("{}", format!("Error: {}", err).to_string().red());
-        }
-        sleep(Duration::from_secs(args.interval)).await;
-    }
+    let whirlpool_pubkey = Pubkey::from_str("8wXA3oeY8EUpmHu2yqzr6k2WJEodTFLuKqTmoQJtM6wP").unwrap();
+    let lower_price = 0.6;
+    let upper_price = 0.61;
+
+    // let liquidity_delta = convert_to_liquidity_delta(1, true)?;
+    // let param = IncreaseLiquidityParam::TokenB(1);
+    let param = IncreaseLiquidityParam::TokenB(1);
+
+    let slippage_tolerance_bps = Some(100);
+
+    // let wallet = Keypair::new();
+    // let funder = Some(wallet.pubkey());
+    // funder = Some(wallet.pubkey());
+    let wallet_pub_key = wallet.pubkey();
+    let result = open_position_instructions(
+        &rpc,
+        whirlpool_pubkey,
+        lower_price,
+        upper_price,
+        param,
+        slippage_tolerance_bps,
+        None,
+     )
+     .await;
+
+    println!("Result: {:?}", result);
+
+    // println!("Position Mint: {:?}", result.position_mint);
+    // println!("Initialization Cost: {} lamports", result.initialization_cost);
+
+
+    // arguments are
+    // rpc: &RpcClient,
+    // pool_address: &Pubkey,
+    // lower_price: f64,
+    // upper_price: f64,
+    // param: IncreaseLiquidityParam,
+    // slippage_tolerance_bps: Option<u16>,
+    // funder: Option<Pubkey>,
+    // let open_position_instructions = open_position_instructions(
+    //     rpc,
+    //     whirlpool_address,
+    //     new_lower_price,
+    //     new_upper_price,
+    //     increase_liquidity_param,
+    //     Some(100),
+    //     None,
+    // )
+
+    // // Replace these with the actual values relevant to your use case.
+    // let token_a_amount: u64 = 1_000_000; // amount of token A you wish to deposit
+    // let token_b_amount: u64 = 500_000;   // amount of token B you wish to deposit
+    // let liquidity_amount: u128 = 100;    // liquidity amount to add
+    // let token_min_a: u64 = 990_000;      // minimum amount of token A acceptable (to account for slippage)
+    // let token_min_b: u64 = 495_000;      // minimum amount of token B acceptable (to account for slippage)
+
+    // // If required, you may also need to specify additional parameters such as tick indexes:
+    // // let tick_lower: i32 = ...;
+    // // let tick_upper: i32 = ...;
+
+    // let close_position_instructions = close_position_instructions(
+    //     &rpc,
+    //     &position.position_mint,
+    //     Some(args.slippage_tolerance_bps),
+    //     None,
+    // )
+
+
+    // let open_position_instructions = open_position_instructions(
+    //     rpc,
+    //     position_address,
+    //     1,
+    //     1.1,
+    //     increase_liquidity_param,
+    //     Some(100),
+    //     None,
+    // )
+
+    // loop {
+    //     if let Err(err) = run_position_manager(
+    //         &rpc,
+    //         &args,
+    //         &wallet,
+    //         &mut position,
+    //         &token_mint_a,
+    //         &token_mint_b,
+    //     )
+    //     .await
+    //     {
+    //         eprintln!("{}", format!("Error: {}", err).to_string().red());
+    //     }
+    //     sleep(Duration::from_secs(args.interval)).await;
+    // }
 }
